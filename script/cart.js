cb478d5a681100380a49e6c1ce3ca6c81f8f9c7f
nathallye <nathallyet@gmail.com>
2024-07-30 20:07:26 -0700

    merge

---
+++ script/cart.js
@@ -0,0 +1,20 @@
+// const [quantity, setQuantity] = useState(1);
+
+// const increaseHandler = () => {
+//   setQuantity(state => state + 1);
+// };
+
+// const decreaseHandler = () => {
+//   setQuantity(state => state - 1);
+// };
+
+// const addToCartHandler = () => {
+//   const coffeeToAdd = {
+//     ...coffee,
+//     quantity: quantity,
+//   };
+
+//   addCoffeeToCart(coffeeToAdd);
+// };
+
+// const formatedPrice = formatMoney(coffee.price > 0 ? coffee.price : 0.00);
